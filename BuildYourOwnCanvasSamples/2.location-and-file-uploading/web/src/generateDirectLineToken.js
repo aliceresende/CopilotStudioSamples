f65f1899cdc03e79b7038663abb20763524986e0
1211518 <1211518@isep.ipp.pt>
2024-04-19 14:13:32 +0100

    Canvas updates

--- BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
+++ BuildYourOwnCanvasSamples/2.location-and-file-uploading/web/src/generateDirectLineToken.js
@@ -7,7 +7,7 @@ module.exports = async function generateDirectLineToken(botId, botTenantId) {
   
   // The URL host in which to generate the Direct Line token is subject to change based off of environment.
   // The host should be consistent to the host that you are building your Power VA.
-  const response = await fetch('https://va.ai.dynamics.com/api/botmanagement/v1/directline/directlinetoken?botId=' + botId + '&tenantId=' + botTenantId, {method: "GET"});
+  const response = await fetch(' https://a5693f94fce2e951b6b8257b9c1e97.4a.environment.api.powerplatform.com/powervirtualagents/botsbyschema/new_helper/directline/token?api-version=2022-03-01-preview' + botId + '&tenantId=' + botTenantId, {method: "GET"});
   
   const { token } = await response.json();
 
